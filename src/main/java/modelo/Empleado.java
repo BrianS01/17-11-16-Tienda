a415ebde27c8ab4a3715a9e6814ac717c1f81189
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-16 10:40:13 -0500

    Tienda

--- src/main/java/modelo/Empleado.java
+++ src/main/java/modelo/Empleado.java
@@ -1,14 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
+
 package modelo;
 
-/**
- *
- * @author BRIAN
- */
-public class Empleado {
-    
+public class Empleado
+{
+    private int idEmpleado;
+    private String nombre;
+
+    public int getIdEmpleado()
+    {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado)
+    {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Empleado{" + "idEmpleado=" + idEmpleado + ", nombre=" + nombre + '}';
+    }
 }
