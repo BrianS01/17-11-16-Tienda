e0938145d88932990e5d063c8a39a2a47b361ca8
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-16 10:23:37 -0500

    Tienda

--- src/main/java/controlador/ProductoControlador.java
+++ src/main/java/controlador/ProductoControlador.java
@@ -1,14 +1,85 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
 package controlador;
+        
+import java.io.IOException;
 
-/**
- *
- * @author BRIAN
- */
-public class ProductoControlador {
-    
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.ProductoDAO;
+import modelo.Producto;
+
+public class ProductoControlador extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    private static String INSERT_OR_EDIT = "/user.jsp";
+    private static String LIST_USER = "/listUser.jsp";
+    private ProductoDAO dao;
+
+    public ProductoControlador()
+    {
+        super();
+        dao = new ProductoDAO();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String forward = "";
+        String action = request.getParameter("action");
+        if (action.equalsIgnoreCase("delete"))
+        {
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            dao.deleteUser(userId);
+            forward = LIST_USER;
+            request.setAttribute("artista", dao.getAllUsers());
+        }
+        else if (action.equalsIgnoreCase("edit"))
+        {
+            forward = INSERT_OR_EDIT;
+            int userId = Integer.parseInt(request.getParameter("cedula"));
+            Producto user = dao.getUserById(userId);
+            request.setAttribute("user", user);
+        }
+        else if (action.equalsIgnoreCase("listUser"))
+        {
+            forward = LIST_USER;
+            request.setAttribute("artista", dao.getAllUsers());
+        }
+        else
+        {
+            forward = INSERT_OR_EDIT;
+        }
+
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        Producto user = new Producto();
+        user.setNombre(request.getParameter("nombre"));
+        String edad = request.getParameter("edad");
+        user.setObra(request.getParameter("obra"));
+        user.setEstilo(request.getParameter("estilo"));
+        String cedula = request.getParameter("cedula");
+        if (cedula == null || cedula.isEmpty())
+        {
+            dao.addUser(user);
+        }
+        else
+        {
+            user.setCedula(Integer.parseInt(cedula));
+            dao.updateUser(user);
+        }
+        
+        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
+        request.setAttribute("users", dao.getAllUsers());
+        view.forward(request, response);
+    }
 }
